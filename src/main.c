61fe2e36a1b64b1438b74614d84e078f4b9d790e
Sam Smith <sams96@mail.com>
2019-11-26 14:21:44 +0800

    Added list view

--- src/main.c
+++ src/main.c
@@ -23,9 +23,45 @@ static void activate (GtkApplication * app, gpointer user_data)
 {
 	GtkWidget * window;
 
+	GtkCellRenderer *renderer;
+	GtkTreeViewColumn *column;
+	GtkListStore *store; 
+	GtkWidget *list;
+
+	GtkWidget *box;
+	GtkWidget *label;
+	GtkTreeSelection *selection;
+
+	store = gtk_list_store_new(1, G_TYPE_UINT);
+
 	window = gtk_application_window_new(app);
+	list = gtk_tree_view_new();
+
+	/* Create column with cell renderer */
+	renderer = gtk_cell_renderer_text_new ();
+	column = gtk_tree_view_column_new_with_attributes("ID",
+		  renderer, "text", 0, NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+
+	gtk_tree_view_set_model(GTK_TREE_VIEW(list), GTK_TREE_MODEL(store));
+
+	g_object_unref(store);
+
 	gtk_window_set_title(GTK_WINDOW(window), "uelp");
 	gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(list), FALSE);
+
+	box = gtk_box_new(FALSE, 0);
+
+	gtk_box_pack_start(GTK_BOX(box), list, TRUE, TRUE, 5);
+
+	label = gtk_label_new("");
+	gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 5);
+
+	gtk_container_add(GTK_CONTAINER(window), box);
+
 	gtk_widget_show_all(window);
 }
 
