5f07cfd28c0521ce767d14080a828c3361b0d030
Sam Smith <sams96@mail.com>
2019-11-28 01:42:35 +0800

    Added '+' button, which currently just causes a segfault, and 2 columns to the database

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ uelp: src/main.c
 	gcc -o uelp src/main.c -lsqlite3 `pkg-config --cflags --libs gtk+-3.0`
 
 clean:
-	rm uelp
+	rm uelp books.db
--- src/main.c
+++ src/main.c
@@ -21,6 +21,12 @@
 #include <gtk/gtk.h>
 #include <sqlite3.h>
 
+typedef struct uelp_t
+{
+	sqlite3 * db;
+	GtkListStore * store;
+} uelp_t;
+
 static char * default_db = "books.db";
 
 int db_add_item_to_gtk_store_callback (void * model, int argc, char ** argv,
@@ -28,78 +34,110 @@ int db_add_item_to_gtk_store_callback (void * model, int argc, char ** argv,
 {
 	GtkTreeIter i;
 	gtk_list_store_append(GTK_LIST_STORE(model), &i);
-	gtk_list_store_set(GTK_LIST_STORE(model), &i, 0, argv[0], -1);
+	gtk_list_store_set(GTK_LIST_STORE(model), &i, 0, argv[0], 
+			1, argv[1],
+			2, argv[2],
+			-1);
 
 	return 0;
+}
+
+static void add_button_pressed (GtkWidget * widget, gpointer user_data)
+{
+	int status;
+	char * error_msg;
+	uelp_t * main = (uelp_t *) user_data;
+
+	status = sqlite3_exec(main->db,
+			"INSERT INTO Books(title, author) Values('w', 'v')",
+			db_add_item_to_gtk_store_callback, main->store, &error_msg);
+
+	if (status != SQLITE_OK) {
+		fprintf(stderr, "Cannot query database, SQL error: %s\n", error_msg);
+
+		sqlite3_free(error_msg);
+		sqlite3_close((sqlite3 *) user_data);
+		exit(status);
+	}
 }
 
 static void activate (GtkApplication * app, gpointer user_data)
 {
 	GtkWidget * window;
+	uelp_t * main;
 
 	GtkCellRenderer * renderer;
 	GtkTreeViewColumn * column;
-	GtkListStore * store;
 	GtkWidget * list;
 
 	GtkWidget * box;
-	GtkWidget * label;
+	GtkWidget * button;
 	GtkTreeSelection * selection;
 
-	sqlite3 * db;
 	char * error_msg;
 	int status;
 
 	/* Open DB */
-	store = gtk_list_store_new(1, G_TYPE_UINT);
-	status = sqlite3_open(default_db, &db);
+	main->store = gtk_list_store_new(3, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+	status = sqlite3_open(default_db, &main->db);
 
 	if (status != SQLITE_OK) {
 		fprintf(stderr, "Cannot open database, SQL error: %s\n",
-				sqlite3_errmsg(db));
-		sqlite3_close(db);
+				sqlite3_errmsg(main->db));
+		sqlite3_close(main->db);
 		exit(status);
 	}
 
 	/* Create table if it doesn't already exist */
-	status = sqlite3_exec(db,
-			"CREATE TABLE IF NOT EXISTS Books(Id INT PRIMARY KEY)",
+	status = sqlite3_exec(main->db,
+			"CREATE TABLE IF NOT EXISTS "
+		   		"Books(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+					"Title TEXT, "
+					"Author TEXT)",
 			0, 0, &error_msg);
 
 	if (status != SQLITE_OK) {
 		fprintf(stderr, "Cannot create table, SQL error: %s\n", error_msg);
 
 		sqlite3_free(error_msg);
-		sqlite3_close(db);
+		sqlite3_close(main->db);
 		exit(status);
 	}
 
 	/* Load database into GTK store */
-	status = sqlite3_exec(db, "SELECT * FROM Books",
-			db_add_item_to_gtk_store_callback, store, &error_msg);
+	status = sqlite3_exec(main->db, "SELECT * FROM Books",
+			db_add_item_to_gtk_store_callback, main->store, &error_msg);
 
 	if (status != SQLITE_OK) {
 		fprintf(stderr, "Cannot query database, SQL error: %s\n", error_msg);
 
 		sqlite3_free(error_msg);
-		sqlite3_close(db);
+		sqlite3_close(main->db);
 		exit(status);
 	}
-
-	sqlite3_close(db);
 
 	window = gtk_application_window_new(app);
 	list = gtk_tree_view_new();
 
 	/* Create column with cell renderer */
-	renderer = gtk_cell_renderer_text_new ();
-	column = gtk_tree_view_column_new_with_attributes("ID", renderer, "text", 0,
-			NULL);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes("ID", renderer, "text",
+			0, NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
 
-	gtk_tree_view_set_model(GTK_TREE_VIEW(list), GTK_TREE_MODEL(store));
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes("Title", renderer, "text",
+			1, NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
 
-	g_object_unref(store);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes("Author", renderer, "text",
+			2, NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+
+	gtk_tree_view_set_model(GTK_TREE_VIEW(list), GTK_TREE_MODEL(main->store));
+
+	g_object_unref(main->store);
 
 	gtk_window_set_title(GTK_WINDOW(window), "uelp");
 	gtk_window_set_default_size(GTK_WINDOW(window), 960, 720);
@@ -107,16 +145,20 @@ static void activate (GtkApplication * app, gpointer user_data)
 
 	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(list), FALSE);
 
-	box = gtk_box_new(FALSE, 0);
+	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
 	gtk_box_pack_start(GTK_BOX(box), list, TRUE, TRUE, 5);
 
-	label = gtk_label_new("");
-	gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 5);
+	button = gtk_button_new_with_label("+");
+	g_signal_connect(G_OBJECT(button), "clicked",
+			G_CALLBACK(add_button_pressed), main);
+	gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 5);
 
 	gtk_container_add(GTK_CONTAINER(window), box);
 
 	gtk_widget_show_all(window);
+
+	sqlite3_close(main->db);
 }
 
 int main (int argc, char * argv[])
