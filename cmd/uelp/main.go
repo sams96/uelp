7d7f8627500121fef83b6d09cdc163cc74531426
Sam Smith <sams96@mail.com>
2020-02-05 16:48:03 +0000

    Initial go gtk test

---
+++ cmd/uelp/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"github.com/gotk3/gotk3/gtk"
+	"os"
+)
+
+const WindowName = "window"
+const UIMain = "glade/main.glade"
+
+func main() {
+	gtk.Init(&os.Args)
+	bldr, err := getBuilder(UIMain)
+	if err != nil {
+		panic(err)
+	}
+
+	window, err := getWindow(bldr)
+	if err != nil {
+		panic(err)
+	}
+
+	window.SetTitle("uelp")
+	window.SetDefaultSize(640, 480)
+	_, err = window.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	window.ShowAll()
+
+	gtk.Main()
+}
+
+func getBuilder(filename string) (*gtk.Builder, error) {
+	b, err := gtk.BuilderNew()
+	if err != nil {
+		return nil, err
+	}
+
+	if filename != "" {
+		err = b.AddFromFile(filename)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return b, nil
+}
+
+func getWindow(b *gtk.Builder) (*gtk.Window, error) {
+	obj, err := b.GetObject(WindowName)
+	if err != nil {
+		return nil, err
+	}
+
+	window, ok := obj.(*gtk.Window)
+	if !ok {
+		return nil, err
+	}
+
+	return window, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sams96/uelp
+
+go 1.13
+
+require github.com/gotk3/gotk3 v0.0.0-20200129211157-69caac910e50
